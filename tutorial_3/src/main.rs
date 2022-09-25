f10b707b74233580967e16a32dbd2be78c46c843
David Klein <david@kleincyber.com>
2022-09-25 01:59:58 -0400

    fixes

--- tutorial_3/src/main.rs
+++ tutorial_3/src/main.rs
@@ -5,15 +5,14 @@ fn main() {
     {
         let x = 2;
         println!("x is: {}", x);
-
     }
 
     let x = "Hello";
     println!("x is: {}", x); // test comment
 
     // CONsTS
-    const seconds_in_minute: u128 = 60; // Consts are to be defined in capital snake case
+    const SECONDS_IN_MINUTE: u128 = 60; // Consts are to be defined in capital snake case
                                         //
-    println!("x is: {}", seconds_in_minute);
-    println!("x is: {}", seconds_in_minute);
+    println!("x is: {}", SECONDS_IN_MINUTE);
+    println!("x is: {}", SECONDS_IN_MINUTE);
 }
