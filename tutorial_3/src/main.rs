5374ba3aa97cf76cd8014a04650246294637d8ae
David Klein <david@kleincyber.com>
2022-09-25 01:57:45 -0400

    add const

--- tutorial_3/src/main.rs
+++ tutorial_3/src/main.rs
@@ -11,5 +11,9 @@ fn main() {
     let x = "Hello";
     println!("x is: {}", x); // test comment
 
-
+    // CONsTS
+    const seconds_in_minute: u128 = 60; // Consts are to be defined in capital snake case
+                                        //
+    println!("x is: {}", seconds_in_minute);
+    println!("x is: {}", seconds_in_minute);
 }
